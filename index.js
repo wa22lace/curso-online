959334d6c8e4b8dcafa7707875e047c953fcb4f6
wa22lace <48635988+wa22lace@users.noreply.github.com>
2025-01-05 10:20:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+
 var quik = document.getElementsByClassName('quik')
+const butao = document.getElementById('butao')
+
+setTimeout(() =>{butao.click();},4000);
+
+('butao').click
+setTimeout(function(){ document.getElementById('butao').click();},8000);
+
 
 function clique() {
    console.log("clikc feito")
   }
 
-const butao = document.getElementById('butao')
 
-setTimeout(() =>{butao.click();},8000);
