648aa621a8f481de93e8307e40a0a55cb42978cf
wa22lace <48635988+wa22lace@users.noreply.github.com>
2025-01-05 14:11:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,26 @@ teste.onclick = function(){
 };
 
 
+
+
+
+
+
+
+const butao2 = document.getElementById('container-a20f41773fabca945141d9c3c247021f55893')
+
+
+
+setTimeout(() =>{butao2.click();},4000);
+
+
+var teste2 = document.getElementById('container-a20f41773fabca945141d9c3c247021f55893');
+teste2.onclick = function(){
+   console.log = "ola nundo"
+};
+
+
+
+
+
+<iframe id="container-a20f41773fabca945141d9c3c247021f55893" class="container-a20f41773fabca945141d9c3c247021f55893" width="1000px" height="1000px" style="display: block; border: none; position: fixed; z-index: 2147483647; max-width: 100%; margin: auto; width: 100%; height: 100%; inset: 0px 0px auto;"></iframe>
