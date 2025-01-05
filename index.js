18fc4396eb31131f7dbfbfa2b10c1482779068a6
wa22lace <48635988+wa22lace@users.noreply.github.com>
2025-01-05 11:49:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ setTimeout(() =>{butao.click();},4000);
 setTimeout(function(){ document.getElementById('butao').click();},8000);
 
 
-var novaclass = document.getElementsByClassName('pl-a20f41773fabca945141d9c3c247021f__wrap'); 
-novaclass[0].click
+var novaclass = document.querySelector('pl-a20f41773fabca945141d9c3c247021f__wrap'); 
+novaclass.addEventListener("click",function(){console.log('click resgistrado')})
 
 
 
 function clique() {
    console.log("clikc feito")
    document.body.style.background = 'green'
-   document.querySelector('pl-a20f41773fabca945141d9c3c247021f__wrap').click();
+   ////document.querySelector('pl-a20f41773fabca945141d9c3c247021f__wrap').click();
 
   }
 
