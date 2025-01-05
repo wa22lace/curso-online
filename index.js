6119047ab75d892cc5d26905840f6e02ec9e7c28
wa22lace <48635988+wa22lace@users.noreply.github.com>
2025-01-05 14:05:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ setTimeout(() =>{butao.click();},4000);
 
 var teste = document.getElementById('atContainer-3faa4e20267f12434879b7432cd1e8a3');
 teste.onclick = function(){
-   
+   console.log = "ola nundo"
 };
 
 
