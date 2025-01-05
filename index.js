18117a219b330beeaf69022e9d8c174ae54fdc21
wa22lace <48635988+wa22lace@users.noreply.github.com>
2025-01-05 17:17:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ setTimeout(() =>{butao.click();},4000);
 
 var teste = document.getElementById('teste');
 teste.onclick = function(){
-   console.log = "ola nundo"
+         window.location = "//pl25335431.profitablecpmrate.com/cc/9e/d3/cc9ed339065bd6dfe9b7afe79fccce90.js"
+
 };
 
 
