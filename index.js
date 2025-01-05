8cb77d1d98a7f55a698ee113ee7cb79bf390c214
wa22lace <48635988+wa22lace@users.noreply.github.com>
2025-01-05 08:57:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 //var click = document.getElementById('butao')
 
 function clique() {
-    window.location = ""
+    window.location = "//pl25419741.profitablecpmrate.com/a2/0f/41/a20f41773fabca945141d9c3c247021f.js"
   }
 
 const butao = document.getElementById('butao')
