bc834c0288073a872dc5a0831ede5e8cf8d94089
wa22lace <48635988+wa22lace@users.noreply.github.com>
2025-01-05 11:33:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 var quik = document.getElementsByClassName('quik')
 const butao = document.getElementById('butao')
 
@@ -8,8 +7,15 @@ setTimeout(() =>{butao.click();},4000);
 setTimeout(function(){ document.getElementById('butao').click();},8000);
 
 
+var novaclass = document.getElementsByClassName('pl-a20f41773fabca945141d9c3c247021f__wrap'); 
+novaclass[0].click
+
+
+
 function clique() {
-   console.log("clikc trocou de cor")
+   console.log("clikc feito")
    document.body.style.background = 'green'
+   document.querySelector('pl-a20f41773fabca945141d9c3c247021f__wrap').click();
+
   }
 
