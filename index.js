65b64f84f4c682a47fece644afc31634543afed9
wa22lace <48635988+wa22lace@users.noreply.github.com>
2025-01-04 22:42:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+
 //var click = document.getElementById('butao')
 
 function clique() {
-    window.location =  "//pl25335431.profitablecpmrate.com/cc/9e/d3/cc9ed339065bd6dfe9b7afe79fccce90.js"
+    window.location = "//pl25335431.profitablecpmrate.com/cc/9e/d3/cc9ed339065bd6dfe9b7afe79fccce90.js"
   }
 
 const butao = document.getElementById('butao')
