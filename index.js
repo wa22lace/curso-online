0e90a1b5a78c48a735d29324c6f54579c419ba30
wa22lace <48635988+wa22lace@users.noreply.github.com>
2025-01-05 14:22:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const butao = document.getElementById('atContainer-3faa4e20267f12434879b7432cd1e8a3')
+const butao = document.getElementById('teste')
 
 
 
 setTimeout(() =>{butao.click();},4000);
 
 
-var teste = document.getElementById('atContainer-3faa4e20267f12434879b7432cd1e8a3');
+var teste = document.getElementById('teste');
 teste.onclick = function(){
    console.log = "ola nundo"
 };
