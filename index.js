3318d5a5eddb0da0e5f8ac1a2291f29e6c33836b
wa22lace <48635988+wa22lace@users.noreply.github.com>
2025-01-08 08:58:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const butao = document.getElementById('teste')
 
 
 
-setTimeout(() =>{butao.click();},6000);
+setTimeout(() =>{butao.click();},8000);
 
 
 var teste = document.getElementById('teste');
 teste.onclick = function(){
-      //   window.location = "https://www.profitablecpmrate.com/akv60cf7?key=68aa0b605468f42199fdd436db3f9186"
+         window.location = "https://www.profitablecpmrate.com/akv60cf7?key=68aa0b605468f42199fdd436db3f9186"
 
 };
 
