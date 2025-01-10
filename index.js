93680bc9351066221b62552b46e476a4898e1102
wa22lace <48635988+wa22lace@users.noreply.github.com>
2025-01-10 20:53:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ setTimeout(() =>{butao.click();},8000);
 
 var teste = document.getElementById('teste');
 teste.onclick = function(){
-         window.location = "https://www.profitablecpmrate.com/akv60cf7?key=68aa0b605468f42199fdd436db3f9186"
+         window.location = "https://www.profitablecpmrate.com/g6hxacx5?key=5ac3cc2d868e2dfad3b9ff00a3bfe196"
 
 };
 
